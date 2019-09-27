29f9676c08ff974a4058e35a5dd2cb132e0b9749
Ajinkya-Hiwale <iajinkyahiwale@gmail.com>
2019-09-27 23:32:35 +0530

    pom changed

--- src/test/Java/Test/TestRunCukeTest.java
+++ src/test/Java/Test/TestRunCukeTest.java
@@ -1,11 +1,11 @@
-package MyRunner;
+package Test;
 
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/main/java/Features", ////the path of the feature files
+@CucumberOptions(features = "src/test/Java/Features", ////the path of the feature files
 glue = {"stepDefinitions" }, //path of stepdefinition
 format = { "pretty"}, //to generate reports
 monochrome = true, //display the console output in a proper readable format
@@ -13,7 +13,7 @@ strict = true, //it will check if any step is not defined in step definition fil
 dryRun = false ,//to check the mapping is proper between feature file and step def file
 tags = { "@ErrorNegativeScenarios" })
 
-public class TestRunner {
+public class TestRunCukeTest {
 
 }
 
