d0011b00f0d39eb59a4bcf78a00d844ddb94df99
Ajinkya-Hiwale <iajinkyahiwale@gmail.com>
2019-10-01 11:17:38 +0530

    POM modified

--- src/test/Java/Test/RunTesterClass.java
+++ src/test/Java/Test/RunTesterClass.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/Java/Features", ////the path of the feature files
 glue = {"stepDefinitions" }, //path of stepdefinition
-format = { "pretty"}, //to generate reports
+format = { "pretty","html:target/cucumber-html-report","json:target/cucumber.json"}, //to generate reports
 monochrome = true, //display the console output in a proper readable format
 strict = true, //it will check if any step is not defined in step definition file
 dryRun = false,tags= {"@TestFeature"}
---
+++ target/cucumber-html-report/report.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Herokuapp.feature");
+formatter.feature({
+  "comments": [
+    {
+      "line": 1,
+      "value": "#Features are nothing but are the test cases"
+    },
+    {
+      "line": 2,
+      "value": "#Feature:In another word it is test scenario name"
+    },
+    {
+      "line": 3,
+      "value": "#Scenario Outline:It is a Test case name"
+    },
+    {
+      "line": 4,
+      "value": "#Below three are called Gherkin keywords"
+    },
+    {
+      "line": 5,
+      "value": "#Given-Given data"
+    },
+    {
+      "line": 6,
+      "value": "#When- checking condition"
+    },
+    {
+      "line": 7,
+      "value": "#Then - what should be the result"
+    },
+    {
+      "line": 8,
+      "value": "#Examples - are used to provide test data as a key and value pair"
+    }
+  ],
+  "line": 10,
+  "name": "TechVerito herokuapp Testing Scenarios",
+  "description": "",
+  "id": "techverito-herokuapp-testing-scenarios",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 9,
+      "name": "@FunctionalTest"
+    }
+  ]
+});
+formatter.before({
+  "duration": 36130951663,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "Testing Error and Negative Scenarios",
+  "description": "",
+  "id": "techverito-herokuapp-testing-scenarios;testing-error-and-negative-scenarios",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 14,
+      "name": "@TestFeature"
+    }
+  ]
+});
+formatter.step({
+  "line": 16,
+  "name": "user is on the page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Herokuapp_StepDefinition.user_is_on_the_page()"
+});
+formatter.result({
+  "duration": 7699457744,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 901641544,
+  "status": "passed"
+});
+});
