2699c3a5ba21bc5e2b8719c874e77f134566cbd8
Ajinkya-Hiwale <iajinkyahiwale@gmail.com>
2019-10-02 14:44:16 +0530

    log4j added

--- src/main/java/stepDefinitions/TestBase.java
+++ src/main/java/stepDefinitions/TestBase.java
@@ -24,7 +24,7 @@ public class TestBase {
 	public FileInputStream fis;
 	public static Properties prop;
 	public static  DesiredCapabilities capabilities;
-	private static Logger Log = Logger.getLogger(TestBase.class);//
+	private static Logger Log = Logger.getLogger(TestBase.class.getName());//
 	
 	public TestBase() throws IOException
 	{
@@ -44,7 +44,7 @@ public class TestBase {
 		String browser=System.getProperty("browser");
 		String version=System.getProperty("version");
 		String platform=System.getProperty("platform");
-		if(browser.equals("chrome"))
+		if(browser.equals("chrome")|| browser.equals(null)||browser.equals("")||browser.equals(" "))
 		{
 			System.setProperty("webdriver.chrome.driver",
 					System.getProperty("user.dir") + "\\src\\main\\java\\driver\\chromedriver.exe");
@@ -56,6 +56,8 @@ public class TestBase {
 			
 			//System.setProperty("webdriver.chrome.driver", "C:/Program Files/chromedriver.exe");
 			driver = new ChromeDriver(capabilities);
+			
+			Log.info("Browser info "+browser+ "Verison info "+capabilities.getVersion() +"Platform is "+capabilities.getPlatform());
 			System.out.println("Browser is "+browser+ " Verison is "+capabilities.getVersion() +"Platform is "+capabilities.getPlatform());
 		}
 		else
@@ -67,6 +69,7 @@ public class TestBase {
 				capabilities.setCapability(CapabilityType.PLATFORM,platform );
 				
 				System.out.println("Browser is "+browser+ " Verison is "+capabilities.getVersion() +"Platform is "+capabilities.getPlatform());				driver = new FirefoxDriver(capabilities);
+				Log.info("Browser is "+browser+ "Verison is "+capabilities.getVersion() +"Platform is "+capabilities.getPlatform());
 			}
 		driver.manage().window().maximize();
 		
