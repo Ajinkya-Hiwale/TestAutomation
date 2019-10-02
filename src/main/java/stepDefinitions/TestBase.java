455a743b831d9047d8092c22b01577d89d54f308
Ajinkya-Hiwale <iajinkyahiwale@gmail.com>
2019-10-02 13:26:55 +0530

    log4j added

--- src/main/java/stepDefinitions/TestBase.java
+++ src/main/java/stepDefinitions/TestBase.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -23,6 +24,7 @@ public class TestBase {
 	public FileInputStream fis;
 	public static Properties prop;
 	public static  DesiredCapabilities capabilities;
+	private static Logger Log = Logger.getLogger(TestBase.class);//
 	
 	public TestBase() throws IOException
 	{
@@ -40,30 +42,31 @@ public class TestBase {
 	{
 		capabilities=new DesiredCapabilities();
 		String browser=System.getProperty("browser");
+		String version=System.getProperty("version");
+		String platform=System.getProperty("platform");
 		if(browser.equals("chrome"))
 		{
 			System.setProperty("webdriver.chrome.driver",
 					System.getProperty("user.dir") + "\\src\\main\\java\\driver\\chromedriver.exe");
-			capabilities.setCapability(CapabilityType.VERSION, System.getProperty("version"));
-			capabilities.setCapability(CapabilityType.PLATFORM, System.getProperty("platform"));
+			capabilities.setCapability(CapabilityType.VERSION, version);
+			capabilities.setCapability(CapabilityType.PLATFORM,platform );
 		
 				
 				
 			
 			//System.setProperty("webdriver.chrome.driver", "C:/Program Files/chromedriver.exe");
 			driver = new ChromeDriver(capabilities);
-			System.out.println("Browser is "+browser+ "Verison is "+System.getProperty("version") +" Platform is "+System.getProperty("platform"));
+			System.out.println("Browser is "+browser+ " Verison is "+capabilities.getVersion() +"Platform is "+capabilities.getPlatform());
 		}
 		else
 			if(browser.equals("ff"))
 			{
 				System.setProperty("webdriver.gecko.driver",
 						System.getProperty("user.dir") + "\\src\\main\\java\\driver\\geckodriver.exe");
-				capabilities.setCapability(CapabilityType.VERSION, System.getProperty("version"));
-				capabilities.setCapability(CapabilityType.PLATFORM, System.getProperty("platform"));
+				capabilities.setCapability(CapabilityType.VERSION, version);
+				capabilities.setCapability(CapabilityType.PLATFORM,platform );
 				
-				System.out.println("Browser is "+browser+ "Verison is "+System.getProperty("version") +" Platform is "+System.getProperty("platform"));
-				driver = new FirefoxDriver(capabilities);
+				System.out.println("Browser is "+browser+ " Verison is "+capabilities.getVersion() +"Platform is "+capabilities.getPlatform());				driver = new FirefoxDriver(capabilities);
 			}
 		driver.manage().window().maximize();
 		
